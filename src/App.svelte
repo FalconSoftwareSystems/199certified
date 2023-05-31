5560a9d60a985edbe3bb04fdef2bef0596156db1
Kartavya Gaur <68074579+CrazyRobotCoder@users.noreply.github.com>
2023-05-31 15:22:13 -0400

    updated files for modular svelte work

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,3 @@
-<script>
-  import fssLogo from './assets/FSS_Logo.png'
-
-  let uname;
-  let pword;
-
-  function showPass() {
-    if (pword.type === "password") {
-      pword.type = "text";
-    } else {
-      pword.type = "password";
-    }
-  }
-</script>
 
 <body>
   <header>
@@ -26,18 +12,5 @@
     </ul>
   </nav>
   
-  <!-- Sign-In Form -->
-  <form class="login">
-    <br><label for="login">Student Login</label><br><br>
-    <label for="login_info">Please enter your MCPS ID and Password</label><br><br>
-  
-    <input type="text" placeholder="Enter MCPS ID" id="uname" bind:this={uname}>
-    <input type="password" placeholder="Enter Password" id="pword" bind:this={pword}><br>
-    <input type="checkbox" on:click={showPass}>Show Password<br>
-  
-    <button type="submit" on:click>Login</button>
-    <!-- <button type="submit" onclick="login()">Login</button> -->
-    <script src="studentvue.js"></script>
-  
-  </form>
+
 </body>
---
+++ src/lib/studentLogin.svelte
@@ -0,0 +1,27 @@
+<script>
+  let uname;
+  let pword;
+
+  function showPass() {
+    if (pword.type === "password") {
+      pword.type = "text";
+    } else {
+      pword.type = "password";
+    }
+  }
+</script>
+  
+   Sign-In Form 
+  <form class="login">
+    <br><label for="login">Student Login</label><br><br>
+    <label for="login_info">Please enter your MCPS ID and Password</label><br><br>
+  
+    <input type="text" placeholder="Enter MCPS ID" id="uname" bind:this={uname}>
+    <input type="password" placeholder="Enter Password" id="pword" bind:this={pword}><br>
+    <input type="checkbox" on:click={showPass}>Show Password<br>
+  
+    <button type="submit" on:click>Login</button>
+    <!-- <button type="submit" onclick="login()">Login</button> -->
+    <script src="studentvue.js"></script>
+  
+  </form>
