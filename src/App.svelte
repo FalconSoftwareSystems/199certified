a4e897dd43f7a95cbe9e377c98762b0aa8221054
bhatchan <aaronbhattachan34@gmail.com>
2023-05-30 23:27:12 -0400

    Re-Add Home Page Content

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,16 @@
-<!--
-
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
-
--->
-
 <script>
   import fssLogo from './assets/FSS_Logo.png'
+
+  let uname;
+  let pword;
+
+  function showPass() {
+    if (pword.type === "password") {
+      pword.type = "text";
+    } else {
+      pword.type = "password";
+    }
+  }
 </script>
 
 <body>
@@ -72,10 +31,11 @@
     <br><label for="login">Student Login</label><br><br>
     <label for="login_info">Please enter your MCPS ID and Password</label><br><br>
   
-    <input type="text" placeholder="Enter Username" id="uname" name="uname">
-    <input type="password" placeholder="Enter Password" id="pword" name="pword"><br>
-    <!-- <input type="checkbox" onclick="showPass()">Show Password<br> -->
+    <input type="text" placeholder="Enter ID" id="uname" bind:this={uname}>
+    <input type="password" placeholder="Enter Password" id="pword" bind:this={pword}><br>
+    <input type="checkbox" on:click={showPass}>Show Password<br>
   
+    <button type="submit" on:click>Login</button>
     <!-- <button type="submit" onclick="login()">Login</button> -->
     <script src="studentvue.js"></script>
   
